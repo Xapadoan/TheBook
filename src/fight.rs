0b72defd9a6e602c502eafea45cbc9f6763e7f70
Xapadoan <HollyolGamu@hotmail.com>
2024-06-14 13:55:36 +0200

    simpler fight

--- src/fight.rs
+++ src/fight.rs
@@ -1,5 +1,6 @@
 use crate::warrior::Warrior;
-use crate::fight_mechanics::{IsDead, IsUnconscious};
+use crate::fight_mechanics::IsUnconscious;
+use crate::weapon::CanHaveWeapon;
 
 #[derive(Debug)]
 pub struct Fight {
@@ -34,48 +35,42 @@ impl Fight {
         (self.blue_corner.name(), self.red_corner.name())
     }
 
+    fn end_reason(loser: &Warrior) -> String {
+        if loser.is_dead() {
+            format!("{} is dead", loser.name())
+        } else if loser.is_unconscious() {
+            format!("{} is unconscious", loser.name())
+        } else {
+            format!("{} has no weapon", loser.name())
+        }
+    }
+
     pub fn auto(mut self) -> FightResult {
         let mut turn: u8 = 0;
 
         self.blue_corner.present_self();
         self.red_corner.present_self();
 
-        let mut end_fight = false;
-        while !end_fight {
+        while turn < u8::MAX {
             println!("=== {turn} ===");
             self.blue_corner.attack(&mut self.red_corner);
             self.red_corner.attack(&mut self.blue_corner);
             println!("\n");
             turn += 1;
-            if self.blue_corner.is_dead() || self.blue_corner.is_unconscious() {
-                end_fight = true;
-            } else if self.red_corner.is_dead() || self.red_corner.is_unconscious() {
-                end_fight = true;
-            } else if turn >= 255 {
-                end_fight = true;
+            if self.blue_corner.is_dead() || self.blue_corner.is_unconscious() || !self.blue_corner.has_weapon() {
+                return FightResult {
+                    winner: Some(self.red_corner),
+                    end_reason: Fight::end_reason(&self.blue_corner),
+                };
+            }
+            if self.red_corner.is_dead() || self.red_corner.is_unconscious() || !self.blue_corner.has_weapon() {
+                return FightResult {
+                    winner: Some(self.blue_corner),
+                    end_reason: Fight::end_reason(&self.red_corner),
+                };
             }
         }
 
-        if turn < 255 {
-            if self.blue_corner.is_dead() || self.blue_corner.is_unconscious() {
-                let winner_name = self.red_corner.name().clone();
-                return FightResult {
-                    winner: Some(self.red_corner),
-                    end_reason: format!("{} winned", winner_name),
-                };
-            }
-            if self.red_corner.is_dead() || self.red_corner.is_unconscious() {
-                let winner_name = self.blue_corner.name().clone();
-                return FightResult {
-                    winner: Some(self.blue_corner),
-                    end_reason: format!("{} winned", winner_name),
-                };
-            }
-            return FightResult {
-                winner: None,
-                end_reason: String::from("both died"),
-            };
-        }
         return FightResult {
             winner: None,
             end_reason: String::from("public got bored and left"),
