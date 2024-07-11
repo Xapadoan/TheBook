3b4dc20aefed2faf7036ac9005d41adee4557019
Xapadoan <HollyolGamu@hotmail.com>
2024-07-11 14:55:10 +0200

    Add tournaments names

--- src/tournament/name.rs
+++ src/tournament/name.rs
@@ -15,8 +15,33 @@ impl RandomDictionary<Name> for TournamentNameDictionary {
     }
 }
 
-const POSSIBLE_NAMES_COUNT: usize = 2;
+const POSSIBLE_NAMES_COUNT: usize = 27;
 const POSSIBLE_NAMES: [&str; POSSIBLE_NAMES_COUNT] = [
     "Blood sausages tournament",
-    "Beer fest of Mliuej",
+    "Beer fest",
+    "Farmer Frenzy",
+    "Farmer Showdown",
+    "Crop Circle Challenge",
+    "Seed Sowing Spectacle",
+    "Harvest Hoedown",
+    "Barnyard Bash",
+    "Farmer Olympics",
+    "Gardening Gala",
+    "Haystack Hootenanny",
+    "Livestock Tournament",
+    "Rural Rumble",
+    "Quack-off",
+    "Farmer Fiesta",
+    "Agrarian Adventure",
+    "Jamboree of Farmers",
+    "Meadows Mayhem",
+    "Countryside Challenge",
+    "Farm Fest Frenzy",
+    "Rural Rumble",
+    "Rustic Revelry",
+    "Pastoral Playoffs",
+    "Homestead Hootenanny",
+    "Fields of Fun",
+    "Country Carnival",
+    "Backwoods Bonanza"
 ];
