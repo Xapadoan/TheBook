354fafe54c42032132ede3254e747e8c051c2a2b
Xapadoan <HollyolGamu@hotmail.com>
2024-08-21 16:52:24 +0200

    Fix rupture resolution show

--- main/src/client/show/show_resolution.rs
+++ main/src/client/show/show_resolution.rs
@@ -3,7 +3,7 @@ use shared::warrior::body::injury::Injury;
 
 pub fn show_rupture(equipment: &dyn Rupture, rupture_damages: u8) -> String {
     if let Some(rup) = equipment.rupture() {
-        if rupture_damages > RUPTURE_MAX || rup + rupture_damages > RUPTURE_MAX {
+        if rupture_damages >= RUPTURE_MAX || rup + rupture_damages >= RUPTURE_MAX {
             String::from("breaks in pieces")
         } else {
             String::from("is damaged")
@@ -24,5 +24,29 @@ pub fn show_lose_finger(possible_injury: &Option<Injury>) -> String {
     match possible_injury {
         Some(_) => String::from(""),
         None => String::from("Luckily, he already lost the finger involved before"),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestRupture {
+        rup: Option<u8>
+    }
+    impl Rupture for TestRupture {
+        fn rupture(&self) -> &Option<u8> {
+            &self.rup
+        }
+        fn set_rupture(&mut self, rup: Option<u8>) {
+            self.rup = rup
+        }
+    }
+
+    #[test]
+    fn show_broken_when_rupture_equals_max() {
+        let item = TestRupture { rup: Some(RUPTURE_MAX - 1) };
+        let result = show_rupture(&item, 1);
+        assert_eq!(result, "breaks in pieces");
     }
 }
--- shared/tests/assault_summary/assault_consequences/apply/weapon_damages.rs
+++ shared/tests/assault_summary/assault_consequences/apply/weapon_damages.rs
@@ -1,6 +1,6 @@
 use crate::common::TestAssailant;
 use shared::assault::assault_consequence::{AssaultConsequences, IndividualConsequences};
-use shared::equipment::rupture::RUPTURE_MAX;
+use shared::equipment::rupture::{Rupture, RUPTURE_MAX};
 use shared::equipment::weapon::{OptionalMutableWeapon, Weapon};
 use shared::random::Random;
 
@@ -30,8 +30,27 @@ pub fn victim_drops_weapon_when_broken() {
     let mut victim = TestAssailant::new();
     let weapon = Weapon::random();
     victim.weapon_mut().replace(weapon);
-    assert_eq!(victim.weapon().is_some(), true, "Assailant has no weapon");
+    assert!(victim.weapon().is_some(), "Victim has no weapon");
 
     consequences.apply(&mut assailant, &mut victim);
-    assert_eq!(victim.weapon().is_none(), true, "Assailant didn't lose his weapon");
+    assert!(victim.weapon().is_none(), "Victim didn't lose his weapon");
+}
+
+#[test]
+pub fn assailant_does_not_drop_weapon_if_only_damaged() {
+    let consequences = AssaultConsequences::new(
+        IndividualConsequences::no_consequences(),
+        IndividualConsequences::damage_weapon(1),
+    );
+    let mut assailant = TestAssailant::new();
+    let mut victim = TestAssailant::new();
+    let weapon = Weapon::random();
+    let weapon_rupture = *weapon.rupture().as_ref().unwrap();
+    assailant.weapon_mut().replace(weapon);
+    assert!(assailant.weapon().is_some(), "Assailant has no weapon");
+    
+    consequences.apply(&mut assailant, &mut victim);
+    assert!(assailant.weapon().is_some(), "Assailant dropped his weapon");
+    let weapon = assailant.weapon().as_ref().unwrap();
+    assert_eq!(*weapon.rupture().as_ref().unwrap(), weapon_rupture);
 }
