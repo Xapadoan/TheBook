f4f72b120a6c3f326340472e4f58648c026d06b5
Xapadoan <HollyolGamu@hotmail.com>
2024-09-05 10:26:39 +0200

    higher rewards

--- server/src/tournament/fight_reward.rs
+++ server/src/tournament/fight_reward.rs
@@ -8,13 +8,13 @@ impl FightReward for Tournament {
     fn fight_reward(&self, round_index: usize) -> u32 {
         let total_rounds = self.number_of_rounds();
         if round_index + 1 < total_rounds / 2 {
-            return 0;
-        } else if round_index + 1 < total_rounds * 3 / 4 {
             return 5;
-        } else if round_index == total_rounds - 2 {
+        } else if round_index + 1 < total_rounds * 3 / 4 {
             return 10;
-        } else {
+        } else if round_index == total_rounds - 2 {
             return 20;
+        } else {
+            return 40;
         }
     }
 }
